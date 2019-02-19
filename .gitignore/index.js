4ea658a8dd0078c4849907d05a1e4fa03a7f3d4a
Eraxis <47780957+Eraxis@users.noreply.github.com>
2019-02-19 16:30:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+var PREFIX = "zu!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
@@ -7,4 +9,29 @@ bot.on("ready", function(){
     console.log("Bot prêt");
 });
 
-bot.login(process.env.TOKEN)
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.username,
+                title: '',
+                fields: [{
+                    name: "Lien d'invitation Discord",
+                    value: 'https://discord.gg/SJcYEhx',
+                    inline: false
+                }]
+            }
+        });
+        break;
+    }
+});
+
+bot.login("process.env.TOKEN");
